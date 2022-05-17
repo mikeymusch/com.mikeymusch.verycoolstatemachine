ed75d46131e97ba5a4f7c8747b78534bc444fbc5
mikeymusch <83678747+mikeymusch@users.noreply.github.com>
2022-05-17 18:06:40 +1000

    basic transition item foldout sketched in editor

--- Editor/TransitionTable/TransitionTableEditor.cs
+++ Editor/TransitionTable/TransitionTableEditor.cs
@@ -20,37 +20,35 @@ namespace com.mikeymusch.verycoolstatemachine
             _transitionTable = (TransitionTable)target;
         }
 
+        
+        
         public override VisualElement CreateInspectorGUI()
         {
             _visualElement = new VisualElement();
 
             LoadAndCloneMarkup();
-            DisplayHeader();
-            //FillBodyWithFromStateElements();
+            FillBodyWithFromStateElements();
 
             return _visualElement;
         }
 
+        
+        
         void LoadAndCloneMarkup()
         {
             VisualTreeAsset visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(
                 "Packages/com.mikeymusch.verycoolstatemachine/Editor/TransitionTable/TransitionTableMarkup.uxml");
             visualTree.CloneTree(_visualElement);
         }
-        void DisplayHeader()
-        {
-            // binds the nicified version of the TransitionTable's name to the header label
-            _visualElement.Q<Label>("header-label").text = ObjectNames.NicifyVariableName(target.name);
-        }
-        
         void FillBodyWithFromStateElements()
         {
             if (!_transitionTable.transitionGroups.Any())
             {
                 // display no states warning
-                return;
             }
 
+            // myButton.SetEnabled(false); - use this to disable a button
+            
             VisualElement body = _visualElement.Q<VisualElement>("body");
         }
     }
